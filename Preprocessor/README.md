4b36cef28c2b8479d3a6e0ef79546f406a032f4c
Fikayomi31 <fikayoogundijo@gmail.com>
2023-07-10 23:42:54 +0100

    practise of preprocessor

--- Preprocessor/README.md
+++ Preprocessor/README.md
@@ -1,1 +1,6 @@
 # C Preprocessor
+This project is a practise work of alx alx low level programming which is on C Preprocessor.
+
+## Task
+0- Object like Macro - header file that defines a macro named SIZE with token of 1024
+directive - working with various examples of macro
--- Preprocessor/directive.c
+++ Preprocessor/directive.c
@@ -11,4 +11,10 @@ int main(void)
     printf("%d\n", n);
     int j = DEMO(3) * DEMO(2);
     printf("%d\n", j);
+    printf("File :%s\n", __FILE__);
+    printf("Date :%s\n", __DATE__);
+    printf("Time %s\n", __TIME__);
+    printf("Line :%d\n", __LINE__);
+    printf("ANSI :%d\n", __STDC__);
+
 }
