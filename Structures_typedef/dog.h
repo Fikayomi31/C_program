ea781537e4817f15091f3262a6e0c3399fc72a8a
Fikayomi31 <fikayoogundijo@gmail.com>
2023-07-12 13:59:37 +0100

    task 0 of alx struct

---
+++ Structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type struct created
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * 
+ * Description: a dog struct created
+*/
+struct dog
+{
+    char *name;
+    char *owner;
+    float age;
+};
+
+#endif
