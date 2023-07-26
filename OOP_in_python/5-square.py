6acadad1a346b04b14a169137ae7356d82489b02
Fikayomi31 <fikayoogundijo@gmail.com>
2023-07-26 23:37:58 +0100

    task 5

---
+++ OOP_in_python/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""Defining a class"""
+
+
+class Square:
+
+    def __init__(self, size=0):
+        """Initializing the class"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """return size"""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """Setting the value of size
+        Args:
+            value: size of the size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size * self.__size
+    
+    def my_print(self):
+        """print square with # character"""
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * self.__size)
