db36cbf2991a5f795ca33bd1bbba93e1701aa440
Fikayomi31 <fikayoogundijo@gmail.com>
2023-07-26 22:53:59 +0100

    task 1 on vscode

---
+++ OOP_in_python/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defining a class"""
+
+
+class Square:
+
+
+    def __init__(self, size):
+        """Initializing the class
+        Args:
+            size: size of the square
+        """
+        self.__size = size
