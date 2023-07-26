2683a66276f3803a455048779d7958a55de6bf6a
Fikayomi31 <fikayoogundijo@gmail.com>
2023-07-26 23:00:19 +0100

    task 2 on vscode

---
+++ OOP_in_python/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Defining a class"""
+
+
+class Square:
+
+    def __init__(self, size=0):
+        """Initializing the class
+        Args:
+            size: size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
